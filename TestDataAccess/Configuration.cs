dc0255cc4cfc19657ddec0e2c0a3a5d1013eb432
Martin Vlacil <75024378+martin-vlacil@users.noreply.github.com>
2022-12-21 14:31:10 +0100

    Update Configuration.cs
    SPACES

--- TestDataAccess/Configuration.cs
+++ TestDataAccess/Configuration.cs
@@ -7,5 +7,5 @@ internal class Configuration
         "Initial Catalog = dmai0920_1028757;" +
         "Persist Security Info=True;" +
         "User ID = dmai0920_1028757;" +
-        "Password=Password1!";
+        "Password = Password1!";
 }
