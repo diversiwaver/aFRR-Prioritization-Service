6907f5f0e4eb529dddb450b96e0661ed86701e2f
Mette Thorhauge <mettekt94@gmail.com>
2022-12-21 13:14:09 +0100

    Configuration and Factory class created
    Configuration class containing the database connection string was created. As well as the factory class that allows us to utilize the factory pattern

---
+++ DataAccessLayer/Configuration.cs
@@ -0,0 +1,11 @@
+﻿namespace DataAccessLayer;
+
+internal static class Configuration
+{
+    internal const string CONNECTION_STRING =
+       "Data Source=hildur.ucn.dk;" +
+       "Initial Catalog=dmai0920_1086320;" +
+       "Persist Security Info=True;" +
+       "User ID=dmai0920_1086320;" +
+       "Password=Password1!";
+}
