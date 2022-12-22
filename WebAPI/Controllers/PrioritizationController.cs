9bc217500d33f5ce247b3387c175cbc139fe51e7
Martin Vlacil <martinvlacil.mv@gmail.com>
2022-12-22 09:09:29 +0100

    smol changes

---
+++ WebAPI/Controllers/PrioritizationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PrioritizationController : ControllerBase
+{
+    private readonly ILogger<PrioritizationController> _logger;
+    private readonly IAssetDataAccess _assetDataAccess;
+
+    public PrioritizationController(ILogger<PrioritizationController> logger, IAssetDataAccess assetDataAccess)
+    {
+        _logger = logger;
+        _assetDataAccess = assetDataAccess;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<SignalDto>>> GetAssetRegulations(IEnumerable<SignalDto> signalDtos)
+    {
+        _logger.LogInformation("GetAssetRegulations method called with IEnumerable of signalDtos: {signalDtos}", signalDtos);
+        return null;
+    }
+}
