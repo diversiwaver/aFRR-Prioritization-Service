baabcbbc227179e95e4bb22cf622b1ae6cb4b766
WrexBG <40688355+WrexBG@users.noreply.github.com>
2022-12-22 14:57:46 +0100

    Implemented AssetPrioritizationModel v1
    Created the first iteration of the AssetPrioritizationModel which when given a list of assets and a threshold will make an optimal selection of the least possible assets needed to meet that threshold.
    
    Co-Authored-By: Mette Juul Thorhauge Sørensen <74910895+mette-thorhauge@users.noreply.github.com>

--- PrioritizationModel/AssetPrioritizationModel.cs
+++ PrioritizationModel/AssetPrioritizationModel.cs
@@ -1,11 +1,36 @@
-﻿using WebAPI.DTOs;
+﻿using DataAccessLayer;
+using DataAccessLayer.Interfaces;
+using DataAccessLayer.Models;
+using PrioritizationService.DTOs;
+using WebAPI.DTOs;
+using WebAPI.DTOs.DTOConverters;
 
 namespace PrioritizationModel;
 
-internal class AssetPrioritizationModel : IPriorizitationModel
+internal class AssetPrioritizationModel : IPrioritizationModel
 {
-    public Task<IEnumerable<SignalDTO>> GetAssetRegulations(SignalDTO signalDTO)
+    public IEnumerable<AssetDTO> GetPrioritizedAssets(IEnumerable<AssetDTO> assets, decimal threshold)
     {
-        throw new NotImplementedException();
+        decimal currentQuantityMw = 0;
+        IEnumerable<AssetDTO> optimalSelection = new List<AssetDTO>();
+
+        foreach(AssetDTO asset in assets)
+        {
+            // If CapacityMw is less than what's needed to meet the threshold, add 100% of it
+            if (currentQuantityMw + asset.CapacityMw <= threshold)
+            {
+                // Creates an iterator block in a method. When you use yield return, you can return a sequence of values one at a time, rather than all at once.
+                asset.RegulationPercentage = 100;
+                yield return asset;
+                currentQuantityMw += asset.CapacityMw;
+            }
+            // Otherwise, calculate how many percent of the CapacityMw is needed to match the exact threshold and break out of the loop
+            else
+            {
+                asset.RegulationPercentage = (threshold - currentQuantityMw) / asset.CapacityMw * 100;
+                yield return asset;
+                break;
+            }
+        }
     }
 }
