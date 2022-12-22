aa074c4a041e1ed98883abcfc8bbd94adafba550
WrexBG <40688355+WrexBG@users.noreply.github.com>
2022-12-22 15:01:57 +0100

    Added Tests for Prioritization Model Controller
    Started on test implementation for the controller
    Created a config to get connection string to the test database
    
    TODO: Figure out the asserts
    Co-Authored-By: Mette Juul Thorhauge Sørensen <74910895+mette-thorhauge@users.noreply.github.com>

---
+++ TestPrioritizationModel/Tests/TestPrioritizationModelController.cs
@@ -0,0 +1,52 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.Interfaces;
+using Microsoft.Extensions.Configuration;
+using PrioritizationModel;
+using WebAPI.DTOs;
+
+namespace TestPrioritizationModel.Tests
+{
+    internal class TestPrioritizationModelController
+    {
+        string _connectionString;
+        IConfiguration _configuration;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddXmlFile("app.config");
+            _configuration = builder.Build();
+
+            _connectionString = _configuration.GetConnectionString("aFRR-Service-DataBase");
+        }
+
+        [TestCase("v1")]
+        [Test]
+        public async Task AssetDataAccess_ShouldGetAllAssets(string version)
+        {
+            //Arrange
+            IAssetDataAccess dataAccess = DataAccessFactory.GetDataAccess<IAssetDataAccess>(_connectionString); // TODO: Use a Stub instead
+            IPrioritizationModel prioritizationModel = PrioritizationModelFactory.GetPrioritizationModel(version);
+            PriotizationModelController priotizationModelController = new(dataAccess, prioritizationModel);
+            SignalDTO signalDTO = new()
+            {
+                Id = 0,
+                FromUtc = DateTime.UtcNow,
+                ToUtc = DateTime.UtcNow.AddHours(1),
+                Price = 25,
+                CurrencyId = 0,
+                QuantityMw = 35,
+                DirectionId = 1,
+                BidId = 0
+            };
+            //Act
+            IEnumerable<SignalDTO> signalsDTOs = await priotizationModelController.GetAssetRegulationsAsync(signalDTO, _configuration);
+
+            //Assert
+            Assert.Multiple(() =>
+            {
+                Assert.Fail("Task failed successfully (TODO: Figure out the asserts and finish the PrioritizationModelController)");
+            });
+        }
+    }
+}
