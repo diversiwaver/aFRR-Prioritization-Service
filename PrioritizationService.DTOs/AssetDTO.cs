254a7ee89d633092abb896d4591741fd2ecaa941
WrexBG <40688355+WrexBG@users.noreply.github.com>
2022-12-22 14:54:30 +0100

    Added DTOs project and AssetDTO
    Added an AssetDTO with RegulationPercentage property
    
    Co-Authored-By: Mette Juul Thorhauge Sørensen <74910895+mette-thorhauge@users.noreply.github.com>

---
+++ PrioritizationService.DTOs/AssetDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Principal;
+
+namespace PrioritizationService.DTOs;
+
+public class AssetDTO
+{
+    public int Id { get; init; }
+    public int AssetGroupId { get; init; }
+    public string Location { get; init; }
+    public int CapacityMw { get; init; }
+    public decimal RegulationPercentage { get; set; }
+}
