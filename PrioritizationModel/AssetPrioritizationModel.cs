08d00e5fac7f3cc65d4a31b977eb445cc7f6f504
WrexBG <40688355+WrexBG@users.noreply.github.com>
2022-12-27 14:18:33 +0100

    Update AssetPrioritizationModel.cs
    Added a null check for the assets and fixed the name of totalCapacityMw as it was named after quantity before.
    Removed unused list and imports.

--- PrioritizationModel/AssetPrioritizationModel.cs
+++ PrioritizationModel/AssetPrioritizationModel.cs
@@ -1,9 +1,4 @@
-﻿using DataAccessLayer;
-using DataAccessLayer.Interfaces;
-using DataAccessLayer.Models;
-using PrioritizationService.DTOs;
-using WebAPI.DTOs;
-using WebAPI.DTOs.DTOConverters;
+﻿using PrioritizationService.DTOs;
 
 namespace PrioritizationModel;
 
@@ -11,23 +6,27 @@ internal class AssetPrioritizationModel : IPrioritizationModel
 {
     public IEnumerable<AssetDTO> GetPrioritizedAssets(IEnumerable<AssetDTO> assets, decimal threshold)
     {
-        decimal currentQuantityMw = 0;
-        IEnumerable<AssetDTO> optimalSelection = new List<AssetDTO>();
+        decimal totalCapacityMw = 0;
 
-        foreach(AssetDTO asset in assets)
+        if (assets == null)
+        {
+            throw new ArgumentNullException(nameof(assets), "The assets collection cannot be null.");
+        }
+
+        foreach (AssetDTO asset in assets)
         {
             // If CapacityMw is less than what's needed to meet the threshold, add 100% of it
-            if (currentQuantityMw + asset.CapacityMw <= threshold)
+            if (totalCapacityMw + asset.CapacityMw <= threshold)
             {
                 // Creates an iterator block in a method. When you use yield return, you can return a sequence of values one at a time, rather than all at once.
                 asset.RegulationPercentage = 100;
                 yield return asset;
-                currentQuantityMw += asset.CapacityMw;
+                totalCapacityMw += asset.CapacityMw;
             }
             // Otherwise, calculate how many percent of the CapacityMw is needed to match the exact threshold and break out of the loop
             else
             {
-                asset.RegulationPercentage = (threshold - currentQuantityMw) / asset.CapacityMw * 100;
+                asset.RegulationPercentage = (threshold - totalCapacityMw) / asset.CapacityMw * 100;
                 yield return asset;
                 break;
             }
