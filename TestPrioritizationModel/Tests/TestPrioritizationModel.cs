a4c20aa3913e3f493228270c000b6b0312300991
WrexBG <40688355+WrexBG@users.noreply.github.com>
2022-12-27 14:17:18 +0100

    Added Stub, Test for the model and dependencies
    Added Minver to the DTOs project
    Added a stub for the model
    Added an empty test for the testing the model

---
+++ TestPrioritizationModel/Tests/TestPrioritizationModel.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.Interfaces;
+using Microsoft.Extensions.Configuration;
+using PrioritizationModel;
+using PrioritizationService.DTOs;
+
+namespace TestPrioritizationModel.Tests;
+
+internal class TestPrioritizationModel
+{
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+
+    }
+
+    [Test]
+    public async Task PrioritizationModel_ShouldGetPrioritizedAssets_WhenGivenSignalAndThreshold()
+    {
+
+    }
+}
