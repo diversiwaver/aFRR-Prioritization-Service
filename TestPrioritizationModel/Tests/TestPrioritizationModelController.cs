8f5afd76c5f530ec434dcc27f5828d7db4a85cba
WrexBG <40688355+WrexBG@users.noreply.github.com>
2022-12-28 10:38:58 +0100

    Update TestPrioritizationModelController
    Changed XML based App.config to JSON based appsettings.json which is a more modern approach.
    Added a private AssetDTOComparer as a local class to test for value equivalency.

--- TestPrioritizationModel/Tests/TestPrioritizationModelController.cs
+++ TestPrioritizationModel/Tests/TestPrioritizationModelController.cs
@@ -9,20 +9,20 @@ namespace TestPrioritizationModel.Tests;
 
 internal class TestPrioritizationModelController
 {
-    string _connectionString;
+    string? _connectionString;
     IConfiguration _configuration;
 
     [OneTimeSetUp]
     public void OneTimeSetup()
     {
-        var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddXmlFile(Path.Combine(Directory.GetCurrentDirectory(), "../../../App.config"));
-        _configuration = builder.Build();
+        string basePath = Path.Combine(Directory.GetCurrentDirectory(), "../../../");
+        _configuration = new ConfigurationBuilder().SetBasePath(basePath).AddJsonFile("appsettings.json", optional: false).Build();
 
         _connectionString = _configuration.GetConnectionString("aFRR-Service-DataBase");
     }
 
     [Test]
-    public async Task PrioritizationController_ShouldGetPrioritizedAssets_WhenProvidedAModel()
+    public void PrioritizationController_ShouldGetPrioritizedAssets_WhenProvidedAModel()
     {
         //Arrange
         IAssetDataAccess dataAccess = DataAccessFactory.GetDataAccess<IAssetDataAccess>(_connectionString); // TODO: Use a Stub instead
@@ -39,15 +39,53 @@ internal class TestPrioritizationModelController
             DirectionId = 1,
             BidId = 0
         };
+        IEnumerable<AssetDTO> expectedResults = new List<AssetDTO> {
+            new AssetDTO() {Id = 1, CapacityMw = 10, RegulationPercentage = 100},
+            new AssetDTO() {Id = 2, CapacityMw = 15, RegulationPercentage = 100},
+            new AssetDTO() {Id = 3, CapacityMw = 20, RegulationPercentage = 50}
+        };
+
         //Act
         signalDTO = priotizationModelController.GetAssetRegulationsAsync(signalDTO, _configuration);
 
         //Assert
         Assert.Multiple(() =>
         {
-            Assert.That(signalDTO, Is.Not.Null, "Returned Signal DTO was null!");
-            Assert.That(signalDTO.AssetsToReguate, Is.Not.Empty, "Returned Signal DTO had to assigned assets to regulate!");
-            Assert.Fail("Task failed successfully (TODO: Figure out the asserts and finish the PrioritizationModelController)");
+            Assert.That(signalDTO, Is.Not.Null, "Returned SignalDTO was null!");
+            Assert.That(signalDTO.AssetsToReguate, Is.Not.Empty, "Returned SignalDTO had to assigned assets to regulate!");
+            Assert.That(signalDTO.AssetsToReguate, Is.EquivalentTo(expectedResults).Using(new AssetDTOComparer()), "Returned assets did not match the assets from the stub.");
         });
+    }
+}
+
+internal class AssetDTOComparer : IEqualityComparer<AssetDTO>
+{
+    public bool Equals(AssetDTO x, AssetDTO y)
+    {
+        if (ReferenceEquals(x, y))
+        {
+            return true;
+        }
+
+        if (x == null || y == null)
+        {
+            return false;
+        }
+
+        return x.Id == y.Id
+            && x.AssetGroupId == y.AssetGroupId
+            && x.Location == y.Location
+            && x.CapacityMw == y.CapacityMw
+            && x.RegulationPercentage == y.RegulationPercentage;
+    }
+
+    public int GetHashCode(AssetDTO obj)
+    {
+        // Return a consistent value for objects with the same values
+        return obj.Id.GetHashCode()
+            ^ obj.AssetGroupId.GetHashCode()
+            ^ obj.Location.GetHashCode()
+            ^ obj.CapacityMw.GetHashCode()
+            ^ obj.RegulationPercentage.GetHashCode();
     }
 }
