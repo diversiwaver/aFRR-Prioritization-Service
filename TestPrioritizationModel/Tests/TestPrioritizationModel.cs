619fe7c54bdac34a3e0274edba02b724a85b4454
WrexBG <40688355+WrexBG@users.noreply.github.com>
2022-12-28 13:04:12 +0100

    Update TestPrioritizationModel.cs
    Testing for exception when given an unreachable threshold.

--- TestPrioritizationModel/Tests/TestPrioritizationModel.cs
+++ TestPrioritizationModel/Tests/TestPrioritizationModel.cs
@@ -27,20 +27,28 @@ internal class TestPrioritizationModel
         };
     }
 
-    [TestCase("v1", 999), TestCase("v1", 30), TestCase("v1", 12), TestCase("v1", 4), TestCase("v1", 23.33), TestCase("v1", 0)]
+    [TestCase("v1", 999), TestCase("v1", 30), TestCase("v1", 23.33), TestCase("v1", 12), TestCase("v1", 4), TestCase("v1", 2), TestCase("v1", 0)]
     [Test]
     public void PrioritizationModel_ShouldGetPrioritizedAssets_WhenGivenSignalAndThreshold(string modelVersion, decimal threshold)
     {
         // Assert
         IPrioritizationModel prioritizationModel = PrioritizationModelFactory.GetPrioritizationModel(modelVersion);
-        IEnumerable<AssetDTO> retrievedAssets;
-        decimal totalRegulatedCapacity;
+        IEnumerable<AssetDTO> retrievedAssets = Enumerable.Empty<AssetDTO>();
+        decimal totalRegulatedCapacity = 0;
 
         // Act
-        retrievedAssets = prioritizationModel.GetPrioritizedAssets(_assets, threshold);
-        totalRegulatedCapacity = retrievedAssets.Sum(asset => (asset.RegulationPercentage / 100) * asset.CapacityMw);
+        if (threshold > _assets.Sum(asset => asset.CapacityMw))
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => prioritizationModel.GetPrioritizedAssets(_assets, threshold), "Prioritization Model did not throw exception when given unreachable threshold.");
+        }
+        else
+        {
+            retrievedAssets = prioritizationModel.GetPrioritizedAssets(_assets, threshold);
 
-        // Arrange
-        Assert.That(totalRegulatedCapacity, Is.EqualTo(threshold));
+            totalRegulatedCapacity = retrievedAssets.Sum(asset => (asset.RegulationPercentage / 100) * asset.CapacityMw);
+
+            // Arrange
+            Assert.That(totalRegulatedCapacity, Is.EqualTo(threshold).Within(0.0001));
+        }
     }
 }
