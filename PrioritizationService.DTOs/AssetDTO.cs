924c23244ef269891bed737c33bd7324484ab2c7
WrexBG <40688355+WrexBG@users.noreply.github.com>
2022-12-28 10:36:07 +0100

    Added ToString to AssetDTO
    This change makes it easier to debug and see actual results in failed tests.

--- PrioritizationService.DTOs/AssetDTO.cs
+++ PrioritizationService.DTOs/AssetDTO.cs
@@ -9,4 +9,9 @@ public class AssetDTO
     public string Location { get; init; }
     public int CapacityMw { get; init; }
     public decimal RegulationPercentage { get; set; }
+
+    public override string ToString()
+    {
+        return $"PrioritizationService.DTOs.AssetDTO {{Id: {Id}, AssetGroupId: {AssetGroupId}, Location: {Location}, CapacityMw: {CapacityMw}, RegulationPercentage: {RegulationPercentage}}}";
+    }
 }
