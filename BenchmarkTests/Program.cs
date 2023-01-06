014dade00413833a1ce05760e55c315822effd6d
WrexBG <40688355+WrexBG@users.noreply.github.com>
2023-01-06 15:02:32 +0100

    Create Program.cs
    Basic structure of the console app program

---
+++ BenchmarkTests/Program.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace BenchmarkTests;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Initializing benchmarks");
+    }
+}
