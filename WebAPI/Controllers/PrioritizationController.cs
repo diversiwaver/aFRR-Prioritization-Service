9ecca866a9383dd84ea9b182ae17502250ef6a13
miroslavmarinov <1028757@ucn.dk>
2023-01-21 16:14:16 +0100

    Added special usings

--- WebAPI/Controllers/PrioritizationController.cs
+++ WebAPI/Controllers/PrioritizationController.cs
@@ -1,6 +1,11 @@
 ﻿using aFRRService.DTOs.DTOConverters;
 using Microsoft.AspNetCore.Mvc;
 using PrioritizationModel;
+using SignalDTOPrioritization = PrioritizationService.DTOs.SignalDTO;
+using SignalDTOaFRR = aFRRService.DTOs.SignalDTO;
+using AssetDTOPrioritization = PrioritizationService.DTOs.AssetDTO;
+using AssetDTOaFRR = aFRRService.DTOs.AssetDTO;
+
 
 namespace WebAPI.Controllers;
 
@@ -18,15 +23,19 @@ public class PrioritizationController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<aFRRService.DTOs.SignalDTO>> GetAssetRegulations(aFRRService.DTOs.SignalDTO signalDTO)
+    public async Task<ActionResult<SignalDTOaFRR>> GetAssetRegulations(SignalDTOaFRR signalDTO)
     {
         _logger.LogInformation("GetAssetRegulations method called for signalDto: {signalDTO}", signalDTO);
-        PrioritizationService.DTOs.SignalDTO signal = DTOConverter<aFRRService.DTOs.SignalDTO, PrioritizationService.DTOs.SignalDTO>.From(signalDTO);
+
+        SignalDTOPrioritization signal = DTOConverter<SignalDTOaFRR, SignalDTOPrioritization>.From(signalDTO);
         _logger.LogInformation("Converted DTO to signal");
+
         signal = _prioritizationModelController.GetAssetRegulationsAsync(signal);
         _logger.LogInformation("Prioritized {Count} assets to regulate for Signal Id: {Id}", signal.AssetsToRegulate.Count(), signal.Id);
-        signalDTO.AssetsToRegulate = DTOConverter<PrioritizationService.DTOs.AssetDTO, aFRRService.DTOs.AssetDTO>.FromList(signal.AssetsToRegulate);
+
+        signalDTO.AssetsToRegulate = DTOConverter<AssetDTOPrioritization, AssetDTOaFRR>.FromList(signal.AssetsToRegulate);
         _logger.LogInformation("Converted signal to DTO");
+
         return Ok(signalDTO);
     }
 }
